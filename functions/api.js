345c5e01a779964e44fc55b9a957567e3866d212
unknown <damian_morales_utn@hotmail.com>
2018-06-21 00:30:18 -0300

    functions add

---
+++ functions/api.js
@@ -0,0 +1,80 @@
+
+const express = require('express');
+
+var functions = require('firebase-functions');
+const app = express(); //this represent our aplication
+
+
+app.use(express.json());
+var courses =[
+  {
+     name:"English",
+     id:1
+  },
+  {
+     name:"Math",
+     id:2
+  },
+  {
+     name:"Programming",
+     id:3
+  },
+  {
+    name:"Chemestry",
+    id:4
+ }
+ 
+
+];
+
+
+
+exports.widgets = functions.https.onRequest(app);
+
+//routes
+app.get('/', function(req, res) {
+    res.type('text');
+    res.send('Hello World!!!');
+  });
+
+  app.get('/courses', function(req, res) {
+    res.type('application/json');
+    res.send(courses);
+  });
+
+  app.get('/courses/:id', function(req, res) {
+    res.type('application/json');
+    res.send(courses.find( course => course.id  === parseInt(req.params.id)));
+  
+  });
+
+  app.post('/courses',function(req,res){
+    var course = {name: req.body.name , id: courses.length + 1};
+    courses.push(course);
+    res.send(course);
+    console.log(req.body);
+  });
+
+  app.put('/courses',function(req,res){ 
+    var course = courses.find( course => course.id === parseInt(req.params.id)); //find element
+    courses = courses.filter( elto => elto.id !== course.id );// elimino elemento
+    
+    //actualizo atributos
+    course.name = req.body.name;
+    //lo agrego
+    courses.push(course);
+    res.send(course);
+    });
+
+ 
+ 
+  app.delete('/courses',function(req,res){
+    
+    courses = courses.filter( elto => elto.id !== course.id );// elimino elemento
+    res.send(courses);
+    });
+
+  
+
+    
+   

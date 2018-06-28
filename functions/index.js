c844a12a98a84f58f53c08e091c126c386994d98
unknown <damian_morales_utn@hotmail.com>
2018-06-28 01:00:39 -0300

    script 4

--- functions/index.js
+++ functions/index.js
@@ -5,7 +5,7 @@ var app = express();
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-var engines = require('consolidate');;
+var engines = require('consolidate');
 
 
 
